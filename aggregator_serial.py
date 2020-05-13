0aff3a47e5c5c74aa6e79b06742c8d8c399d40d0
Christian Rohner <christian.rohner@it.uu.se>
2020-05-13 08:08:40 +0200

    aggregator, initial checkin

---
+++ aggregator_serial.py
@@ -0,0 +1,16 @@
+import serial
+import time
+
+def readlineCR(port):
+    rv = ""
+    while True:
+        ch = port.read()
+        rv += ch
+        if ch=='\r' or ch=='':
+            return rv
+
+port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=3.0)
+
+while True:
+    rcv = readlineCR(port)
+    print("\r\nreceived:" + repr(rcv))
